2b6ef15c9c892eee36418c680067468225d679ad
LeRoy Dahl <ldahl2018@gmail.com>
2020-10-11 23:04:57 -0500

    base mode finished

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,10 +1,63 @@
-console.log('hello from js');
-
 $(document).ready(onReady);
-//declare variable to hold the operator sign entered by the user
-let operator = '';
 
 function onReady() {
+    // event listeners
+    $('#submit').on('click', calculate);
+    $('#clearInputs').on('click', clearInput);
+    $('.operator').on('click', getOperator);
+} // end onReady
 
+let operator = '';
 
-}
+// updates our global operator variable from user input on DOM
+function getOperator(){
+    operator = $(this).text();
+} // end getOperator
+
+// a function to clear the input fields on DOM
+function clearInput() {
+    $('#numInput1').val('');
+    $('#numInput2').val('');
+}// end clearInput
+
+// POST new calculation object to server and for calculation 
+function calculate(){
+    // checks to see that all input fields are inputted
+    if($('#numInput1').val() == '' || $('#numInput2').val() == ''){
+        alert('Please enter numbers for calulation!')
+        return false;
+    }
+    let numbers = {
+        inputOne: $('#numInput1').val(),
+        operator: operator,
+        inputTwo: $('#numInput2').val(),
+        answer: null
+    }
+    $.ajax({
+        method: 'POST',
+        url: '/calculator',
+        data: numbers
+    }).then(function(response){
+        // a GET route to grab the data from the server and to append to DOM
+            $('#history').empty();
+            $.ajax({
+                method: 'GET',
+                url: '/calculator'
+            }).then(function(response){
+                $('#history').empty();
+                // append new calculation object to the DOM in history list
+                for (let result of response) {
+                    $('#history').append(`<li>${result.inputOne}
+                                            ${result.operator}
+                                            ${result.inputTwo} =
+                                            ${result.answer}
+                                            </li>
+                        `)
+                }
+                // empty out current answer out put from previous calculation
+                $('#resultOutput').empty();
+                // append new answer to resultsOutput
+                $('#resultOutput').append(`<p>Answer: ${response[0].answer}</p>`);
+                })
+            })
+}// end calculate
--- server/server.js
+++ server/server.js
@@ -1,9 +1,42 @@
-const express = require ('express');
-const bodyParser = require ('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
 const port = 5000;
 
-app.listen(port, () => {
-    console.log('Up and running on port: ', port);
-});
+app.use(express.static('server/public'));
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+let numberArray = [];
+
+// GET route to send number array data
+app.get('/calculator', (req, res)=>{
+    res.send(numberArray);
+})
+
+// POST calculation result to object in server
+app.post('/calculator', (req, res)=>{
+    numberArray.unshift(req.body);
+    // performing calculation based on operator input
+    switch(numberArray[0].operator) {
+        case `+`:
+            numberArray[0].answer = Number(numberArray[0].inputOne) + Number(numberArray[0].inputTwo);
+            break;
+        case `-`:
+            numberArray[0].answer = Number(numberArray[0].inputOne) - Number(numberArray[0].inputTwo);
+            break;
+        case `*`:
+            numberArray[0].answer = Number(numberArray[0].inputOne) * Number(numberArray[0].inputTwo);
+            break;
+        case `/`:
+            numberArray[0].answer = Number(numberArray[0].inputOne) / Number(numberArray[0].inputTwo);
+            break;
+    }
+    res.sendStatus(200);
+})
+
+app.listen(port, ()=>{
+    console.log('Server up and running at:', port);
+})
